edeed5e40f61ec914fd539ac33992f337b56695c
robw139 <70980290+robw139@users.noreply.github.com>
2022-03-14 18:46:15 -0400

    added index file

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const assertEqual   = require('./assertEqual');
+const eqArrays  = require('./eqArrays');
 
 module.exports = {
   head:   head,
   tail:   tail,
-  middle: middle
+  middle: middle,
+  assertEqual: assertEqual,
+  eqArrays: eqArrays
 };

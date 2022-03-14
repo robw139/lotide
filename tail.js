d66147cfe52769a38907e8b2eb9f598d7810c289
robw139 <70980290+robw139@users.noreply.github.com>
2022-03-14 14:55:30 -0400

    added tailTest.js

--- tail.js
+++ tail.js
@@ -1,11 +1,12 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😀😀😀Assertin Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`👿👿👿Assertin Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`😀😀😀Assertin Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`👿👿👿Assertin Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
+// const assertEqual = require("assertEqual");
 
 const head = function(arr) {
   return (arr[0]);
@@ -15,9 +16,11 @@ const tail = function(arr) {
   return (arr.slice(1,arr.length));
 };
 // test one
-assertEqual(head(["Bootcamp","light","house"]), "Bootcamp");
-assertEqual(head([1,2,3,4]),2);
+// assertEqual(head(["Bootcamp","light","house"]), "Bootcamp");
+// assertEqual(head([1,2,3,4]),2);
 
-let test = [1,2,3,4,5,6,7];
-console.log(tail(test));
-console.log(test);
+// let test = [1,2,3,4,5,6,7];
+// console.log(tail(test));
+// console.log(test);
+
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,5 @@
+const tail = require("../tail")
+
+let test = [1,2,3,4,5,6,7];
+console.log(tail(test));
+console.log(test);
